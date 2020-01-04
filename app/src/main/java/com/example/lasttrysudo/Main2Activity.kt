c51baadb7e55966eca37d32d1304bcd4301f2abb
Katie Lin <chiachen@ualberta.ca>
2020-01-04 13:25:16 -0700

    Back button & Sudoku Grid.

--- app/src/main/java/com/example/lasttrysudo/Main2Activity.kt
+++ app/src/main/java/com/example/lasttrysudo/Main2Activity.kt
@@ -1,12 +1,20 @@
 package com.example.lasttrysudo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        buttonBack.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
--- app/src/main/java/com/example/lasttrysudo/Main3Activity.kt
+++ app/src/main/java/com/example/lasttrysudo/Main3Activity.kt
@@ -1,12 +1,21 @@
 package com.example.lasttrysudo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main3.*
 
 class Main3Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main3)
+
+        buttonBack.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
